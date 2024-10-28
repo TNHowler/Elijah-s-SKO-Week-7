e1f220715a4a1102f78abcde4d4ade3fe7f7ab53
Elijah <s3945008@student.rmit.edu.au>
2024-10-28 11:40:56 +1100

    Add files via upload

--- SKO_7.js
+++ SKO_7.js
@@ -1,8 +1,12 @@
 var elevator;
 var ellipses = [];
+let InterstateBCFont;
+let SoundC
 
 function preload(){
   elevator = loadImage("data/Elevator.jpeg");
+  InterstateBCFont = loadFont("Interstate_BoldCondensed.ttf");
+  SoundC = loadImage("data/SoundC.png");
 }
 
 function setup() {
@@ -12,17 +16,17 @@ function setup() {
     { x: width / 1.1321, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-1/" },
     { x: width / 1.1321, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-3/" },
     { x: width / 1.1321, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-5/" },
-    { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://example.com/5" },
-    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/6" },
-    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://example.com/7" },
+    { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-7/" },
+    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-9/" },
+    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-11/" },
 
     { x: width / 1.0925, y: height / 1.475, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-0/" },
     { x: width / 1.0925, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-2/" },
     { x: width / 1.0925, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-4/" },
     { x: width / 1.0925, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-6/" },
-    { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://example.com/12" },
-    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/13" },
-    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/The-Zipped-Face-Final/" }
+    { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-8/" },
+    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-10/" },
+    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-12/" },
   ];
 }
 
@@ -30,12 +34,18 @@ function draw() {
   background("white");
   image(elevator, 0, 0, windowWidth, windowHeight);
 
+  imageMode(CENTER);
+
+  image(SoundC, width / 2, height / 1.3, width / 3, height / 2.5);
+
+  imageMode(CORNER);
+
   strokeWeight(width / 550);
   noFill();
 
   for (let i = 0; i < ellipses.length; i++) {
     let e = ellipses[i];
-    if (i === 0) {
+    if (i === 4) {
       stroke(0, 255, 0);
     } else if (i === 1) {
       stroke(255, 0, 0);
@@ -44,6 +54,33 @@ function draw() {
     }
     ellipse(e.x, e.y, e.w, e.h);
   }
+
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 50);
+  textFont(InterstateBCFont); 
+  noStroke();
+  fill(0);
+  text(
+    "Codewords Creative Coding and Typography",
+    width / 2.5,
+    height / 10,
+    width / 5, // Specify the width for wrapping the text
+    height / 12,  // Specify the height for wrapping the text
+  );
+
+  // Display text in the middle of the screen
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 115);
+  textFont(InterstateBCFont); 
+  noStroke();
+  fill(0); 
+  text(
+    "In week 7's morning class we briefly looked over our major projects but the main focus was looking at how sound can be used on website. We looked through this website: https://tdingsun.github.io/reading-machines/. The four ways of sound being used that we looked at are: \n\n1. Looping music: A soundtrack that continuously plays while the page is open. \n\n2. Ambient: Ambient sound is like white noise. \n\n3. Generative: This is when an action is inputed by the user and a sound is played. \n\n4. SFX: SFX is similar to generative. \n\nIn the afternoon class we put sound into a website and having it react to the mouse's position. \n\n Click on the code!",
+    width / 2.77,
+    height / 8,
+    width / 3.5, // Specify the width for wrapping the text
+    height / 2  // Specify the height for wrapping the text
+  );
 }
 
 function mousePressed() {
@@ -53,5 +90,13 @@ function mousePressed() {
     if (d < e.w / 2) {
       window.location.href = e.url;
     }
+  }
+
+  let SoundCX = width / 2;  
+  let SoundCY = height / 1.3;  
+  let SoundCW = width / 3;  
+  let SoundCH = height / 2.5;  
+  if (mouseX > SoundCX - SoundCW / 2 && mouseX < SoundCX + SoundCW / 2 && mouseY > SoundCY - SoundCH / 2 && mouseY < SoundCY + SoundCH / 2) {  
+   window.location.href = "https://tnhowler.github.io/Playing-With-Sound/";
   }
 }
